9385850c39160282d8219c122df87722afce9512
Akankshya Mishra <akankshyam@gmail.com>
2024-04-15 18:54:10 -0400

    Updating with side by side scrolling page

--- inconsistency_annotation.md
+++ inconsistency_annotation.md
@@ -1,5 +1,5 @@
-### Task Description
-In this task, you will be presented with a short story, along with a summary of the story. Please read both the story and the summary carefully. You will be asked to assess whether the summary is consistent with the story. We will also include some AI-generated inconsistencies of the summary with respect to the story, which *may or may not* be correct. We will then ask you to evaluate if this generated inconsistency is correct, or if it is incorrect.  Make sure to submit to record your answers.
+#### Task Description
+In this task, you will be presented with a short story, along with a summary of the story. Please read both the story and the summary carefully. We will also include some AI-generated inconsistencies of the summary with respect to the story, which may or may not be correct. We will then ask you to evaluate if this generated inconsistency is correct, or if it is incorrect. Finally, you will be asked to assess whether the summary is consistent with the story. 
 
 **Warning:** Some of the stories may contain content that readers could find offensive, disturbing, or otherwise unsettling. If you do not feel comfortable with this, please feel free to decline the task.
 
@@ -7,7 +7,7 @@ If you encounter any problems or have other feedback, please contact columbia.nl
 
 By participating in this study, you confirm that you are (1) 18 years or older, (2) currently residing in the US, and (3) have read and understand the information above and agree to participate. By clicking submit you consent to participate in this study. If you do not wish to participate, do not click submit.
 
-### Detailed Instructions with Example
+#### Detailed Instructions with Example
 Please read the story carefully and try to understand the points that are central to the plot. 
 
 **Story**
@@ -15,37 +15,21 @@ Please read the story carefully and try to understand the points that are centra
 
 &nbsp;
 
-Below are two example summaries for this story:\
+Two example summaries for this story:
+
 **Summary A**
->The short story describes the execution of six cabinet ministers by firing squad early in the morning during heavy rain. One of the ministers is sick with typhoid fever and too weak to stand, so he is brought downstairs and shot while sitting in a puddle of water with his head on his knees. **The story paints a vivid image of human suffering.**
+>Six cabinet ministers are executed by firing squad early in the morning during heavy rain. One of the ministers is sick with typhoid fever and too weak to stand, so he is shot while **lying in a puddle of water** near the hospital wall.
 
 **Summary B**
->Six cabinet ministers are executed by firing squad early in the morning during heavy rain. One of the ministers is sick with typhoid fever and too weak to stand, so he is shot while **lying in a puddle of water** near the hospital wall.
+>The rain has been pouring heavily, which has led to the hospital shutters being nailed shut. The rainwater has been collecting into pools in the hospital courtyard. The execution of six cabinet ministers is carried out by firing squad early in the morning. **The story paints a vivid image of human suffering.**
 
 &nbsp;
 
+In addition to the story and the summary, there will be AI-generated inconsistencies in the summary, with respect to the story. This would be provided for every story and summary and would contain at least one detected inconsistent detail identified between the summary and the story.
+
 ---
 
-The questions will ask you to evaluate the summary with respect to the story at various levels. Firstly, the questions will ask you to evaluate the summary *line by line*. 
-
-Then, you will be provided with AI-generated inconsistencies of the summary with respect to the story. This would be provided for every story and summary and would contain at least one detected inconsistent detail identified between the summary and the story.
-
-The final question will ask you to classify the entire summary as either consistent, or inconsistent to the story.
-
-##### Question: Is this line in the summary consistent with the story?
-
-This question is asked for *each line of the summary*. Mark each line as either consistent or inconsistent to the story. The events and details described in the summary should not misrepresent details from the story or include details that are unsupported by the story. The lines in the summary that do so can be classified as inconsistent. Some lines include commentary about the story, which should be ignore when evaluating consistency. For such lines, select N/A. For summary A:
-
-* The short story describes the execution of six cabinet ministers by firing squad early in the morning during heavy rain: **Yes**
-* One of the ministers is sick with typhoid fever and too weak to stand, so he is brought downstairs and shot while sitting in a puddle of water with his head on his knees: **Yes**
-* The story paints a vivid image of human suffering: **N/A, just commentary**
-
-For summary B:
-
-* The summary includes the key details of the execution of six cabinet ministers in the early morning during heavy rain: **Yes**
-* The summary includes the key details of the minister sick with typhoid and how he was too weak to stand: **Yes**
-* The summary talks about the minister lying in a puddle of water when shot: **No**
-
+#### Questions
 ##### Question: Evaluate the AI-generated inconsistency.
 
 The information in the *possible AI-generated inconsistency identified in the summary* can contain a correct detected inconsistency in the summary with respect to the story, or contain an incorrect, or invented inconsistency. This question asks you to judge whether this AI-generated inconsistency is correct. You can choose to provide an argument for and against the inconsistency.
@@ -72,15 +56,13 @@ Or, you may choose to answer: "Explain why you think this detected inconsistency
 
 ##### Question: Overall, is the information in the summary consistent with the story?
 
-This question asks you to judge whether or not the entire summary is consistent with the original story. The events and details described in the summary should not misrepresent details from the story or include details that are unsupported by the story.
+This question asks you to judge whether or not the summary is consistent with the original story. In this case, you want to make sure the events and details described in the summary are mentioned in the actual story. 
 
-For example, *Summary A* is consistent with the story. The last bold sentence in *Summary A* contains some commentary on the story, and we ask you to ignore commentary in evaluating consistency. The response would be "Yes" for *Summary A*.
+For example, *Summary A* is **not** consistent with the story as the minister was sitting in the puddle of water and not lying in it. 
 
-*Summary B* is **not** consistent with the story as the minister was sitting in the puddle of water instead of lying in it. The response would be "No" for *Summary B*.
+*Summary B* is consistent with the story. The last bold sentence in *Summary B* contains some commentary on the story, and we ask you to ignore commentary in evaluating consistency. 
 
-An additional input is provided to you for any explanation you may want to provide for your answer. This may be an explanation that all the lines of the summary are consistent with the story, or that one or more lines are inconsistent with the story, or some other explanation.
-
-### Payment
+#### Payment
 For the initial pilot study, you will be paid $35 to evaluate 33 summaries. You will be paid via Upwork once you have completed the pilot. Please allow a few days to submit the payment. 
 
 **Thank you so much for participating in our study!! We would love to hear any feedback that you may have about this task!**
--- inconsistency_annotation.py
+++ inconsistency_annotation.py
@@ -1,37 +1,43 @@
 import os
 
 import streamlit as st
-import streamlit_nested_layout
 import glob
 import json
-import nltk
 import re
 
-query = st.experimental_get_query_params()
+query = st.query_params
 to_download = ""
 if "download" in query:
-    to_download = query["download"][0]
-elif any(["username" not in query, "summaryid" not in query]):
-    st.error("Ill-formatted url. Please enter the url we provided")
-    st.stop()
-else:
-    username = query["username"][0]
-    summary_id = query["summaryid"][0]
+    # We can download all files.
+    annotations = []
+    files = glob.glob(pathname="data/annotations/*/*")
+    for output_name in files:
+        with open(output_name, "r") as file:
+            st.write(output_name)
+            try:
+                annotations.append(json.loads(file.readline()))
+            except:
+                st.write("Failed")
+                continue
 
-if not to_download:
+    btn = st.download_button(
+            label="Download all annotations",
+            data=json.dumps(annotations, indent=2),
+            file_name="annotations.json",
+        )
+elif any(["username" not in query, "summaryid" not in query]):
     # display summarization guidelines
     # load summarization guideline from guideline.md
-    guideline_name = "pandm_annotations.md"
+    guideline_name = "inconsistency_annotation.md"
     with open(guideline_name, "r") as f:
         guideline = f.read()
     st.markdown(guideline)
-    nltk.download('punkt')
-    # Initialize the key in session state
-    if 'clicked' not in st.session_state:
-        st.session_state.clicked = False
-    def clicked():
-        st.session_state.clicked = True
+else:
+    st.set_page_config(layout="wide")
+    username = query["username"]
+    summary_id = query["summaryid"]
 
+    col1, col2 = st.columns(2)
     # open the jsonl containing all source articles into a dictionary
     # each line is a json contains two entries: "id" and "text"
     hashes = ["2268646485413324767", "-9182807582464228672"]
@@ -46,104 +52,78 @@ if not to_download:
     article_text = source[0]['text'].replace('\n', '\n\n')
     summary_text = source[0]['summary']
 
-    st.markdown("### Story")
-    st.markdown(article_text)
-    st.markdown("### Summary")
-    st.markdown(summary_text)
+    with col1.container(height=700):
+        with st.container():
+            st.markdown("### Story")
+            st.markdown(article_text)
+    with col2.container(height=700):
+        with st.container():
+            st.markdown("### Summary")
+            st.markdown(summary_text)
 
-    outfolder = f"data/annotations/{username}"
-    os.makedirs(outfolder, exist_ok=True)
-    output_name = os.path.join(outfolder, f"{summary_id}.jsonl")
-    selected = dict()
+            outfolder = f"data/annotations/{username}"
+            os.makedirs(outfolder, exist_ok=True)
+            output_name = os.path.join(outfolder, f"{summary_id}.jsonl")
 
-    st.markdown(f"### Summary Evaluation")
-    st.markdown("For each line in the summary, evaluate if it is consistent with the story.")
-    st.markdown("Along with the selected input, you can provide an explanation as to why you selected a particular answer, *if you mark the line as inconsistent to the story*. When evaluating, remember that the events and details described in a consistent summary should not misrepresent details from the story or include details that are unsupported by the story.")
-    st.markdown("#### Answers")
-    for i, line in enumerate(nltk.tokenize.sent_tokenize(summary_text)):
-        st.markdown(f"Line {i+1}: {line}")
-        binary_choice_list = ["Yes", "No", "N/A, just commentary"]
-        selected[f"consistent_{i}"] = st.radio(
-            "Is this line in the summary consistent with the story?",
-            key=hash("consistent")+i,
-            options=binary_choice_list,
-            index=None,
-        )
-    st.markdown("---")
-    st.markdown('##### Possible AI-generated inconsistencies detected in summary')
-    regex = r"(^.*)\n+[Reason]"
-    
-    inconsistency_proof = source[0]['response']["choices"][0]["message"]["content"] + "\n" + source[1]['response']["choices"][0]["message"]["content"]
-    selected = dict()
-    if re.search(regex, inconsistency_proof) is not None:
-        matches = re.finditer(regex, inconsistency_proof, re.MULTILINE)
-        for idx, match in enumerate(matches):
-            if len(match.group(1)):
-                st.markdown("Detected " + match.group(1))
+            st.markdown(f"### Summary Evaluation")
+
+            st.markdown('##### Possible AI-generated inconsistencies detected in summary')
+            regex = r"(^.*)\n+[Reason]"
+            
+            inconsistency_proof = source[0]['response']["choices"][0]["message"]["content"] + "\n" + source[1]['response']["choices"][0]["message"]["content"]
+            selected = dict()
+            if re.search(regex, inconsistency_proof) is not None:
+                matches = re.finditer(regex, inconsistency_proof, re.MULTILINE)
+                for idx, match in enumerate(matches):
+                    if len(match.group(1)):
+                        st.markdown("Detected " + match.group(1))
+                        binary_choice_list = ["Explain why you think this detected inconsistency is correct.", "Explain why you think this detected inconsistency is incorrect."]
+                        selected[f"correct_{idx}"] = st.radio(
+                            "",
+                            key=hash("correct")+idx,
+                            options=binary_choice_list,
+                            index=None,
+                            label_visibility="collapsed"
+                        )
+                        if selected[f"correct_{idx}"]:
+                            selected[f"arg_{idx}"] = st.text_area("", key=hash("arg")+idx, label_visibility="collapsed")
+                        st.markdown("---")
+            else:
+                st.markdown("Detected " + inconsistency_proof)
                 binary_choice_list = ["Explain why you think this detected inconsistency is correct.", "Explain why you think this detected inconsistency is incorrect."]
-                selected[f"correct_{idx}"] = st.radio(
+                selected[f"correct"] = st.radio(
                     "",
-                    key=hash("correct")+idx,
+                    key=hash("correct"),
                     options=binary_choice_list,
                     index=None,
                     label_visibility="collapsed"
                 )
-                if selected[f"correct_{idx}"]:
-                    selected[f"arg_{idx}"] = st.text_area("", key=hash("arg")+idx, label_visibility="collapsed")
-    else:
-        st.markdown("Detected " + inconsistency_proof)
-        binary_choice_list = ["Explain why you think this detected inconsistency is correct.", "Explain why you think this detected inconsistency is incorrect."]
-        selected[f"correct"] = st.radio(
-            "",
-            key=hash("correct"),
-            options=binary_choice_list,
-            index=None,
-            label_visibility="collapsed"
-        )
-        if selected[f"correct"]:
-            selected[f"arg"] = st.text_area("", key=hash("arg"), label_visibility="collapsed")
+                if selected[f"correct"]:
+                    selected[f"arg"] = st.text_area("", key=hash("arg"), label_visibility="collapsed")
+                st.markdown("---")
 
-    st.markdown("---")
-    binary_choice_list = ["Yes", "No"]
-    selected["consistent_full"] = st.radio(
-        "Overall, is the information in the summary consistent with the story? "
-        + "The events and details described in a consistent summary should not misrepresent details from the story or include details that are unsupported by the story. ",
-        options=binary_choice_list,
-        index=None,
-    )
-    selected[f"explanation_full"] = st.text_area("Provide an explanation for your selection.")
-    # create a dictionary to store the annotation
-    annotation = {
-        "id": summary_id,
-        "username": username,
-        "story": article_text,
-        "summary": summary_text,
-        "annotation": selected,
-    }
-    # create a submit button and refresh the page when the button is clicked
-    if st.button("Submit", on_click=clicked):
-        os.makedirs("data/annotations", exist_ok=True)
-        with open(output_name, "w") as f:
-            f.write(json.dumps(annotation) + "\n")
-        # display a success message
-        st.success("Annotation submitted successfully!")
+            binary_choice_list = ["Yes", "No"]
+            selected["consistent"] = st.radio(
+                "Overall, is the information in the summary consistent with the story? "
+                + "The events and details described in a consistent summary should not misrepresent details from the story or include details that are unsupported by the story.",
+                options=binary_choice_list,
+                index=None,
+            )
 
-else:
-    # We can download all files.
-    st.write("Only the latest annotations are available.")
-    annotations = []
-    files = glob.glob(pathname="data/annotations/*/*")
-    for output_name in files:
-        with open(output_name, "r") as file:
-            st.write(output_name)
-            try:
-                annotations.append(json.loads(file.readlines()[-1]))
-            except:
-                st.write("Failed")
-                continue
+            # create a dictionary to store the annotation
+            annotation = {
+                "id": summary_id,
+                "username": username,
+                "story": article_text,
+                "summary": summary_text,
+                "annotation": selected,
+            }
 
-    btn = st.download_button(
-            label="Download all annotations",
-            data=json.dumps(annotations, indent=2),
-            file_name="annotations.json",
-        )
+            # create a submit button and refresh the page when the button is clicked
+            if st.button("Submit"):
+                # write the annotation to a json file
+                os.makedirs("data/annotations", exist_ok=True)
+                with open(output_name, "w") as f:
+                    f.write(json.dumps(annotation))
+                # display a success message
+                st.success("Annotation submitted successfully!")
